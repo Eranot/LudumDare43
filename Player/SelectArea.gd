379a7950b89df899a460ac5bdb32c958bdcf83a8
eranot <eranot@eranot-debian.home>
2018-12-02 14:03:24 -0200

    for pull

--- Player/SelectArea.gd
+++ Player/SelectArea.gd
@@ -2,9 +2,7 @@ extends Area2D
 
 var grid_pos_x;
 var grid_pos_y;
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var explosion_obj = preload("res://Player/Explosion.tscn");
 
 var mouse_over = false
 
@@ -21,6 +19,17 @@ func _ready():
 
 func _input(event):
 	if(event is InputEventMouseButton and event.is_pressed() && mouse_over):
+		var node_grid = $"/root/Main/Grid";
+		#node_grid.destroy(grid_pos_x, grid_pos_y)
+		var tile_size = Vector2(64, 64)
+		var half_tile_size = Vector2(32, 32)
+		
+		var new_instance = explosion_obj.instance();
+		new_instance.position = Vector2((grid_pos_x) * tile_size.x + half_tile_size.x, grid_pos_y * tile_size.y + half_tile_size.y);
+		new_instance.grid_pos_x = grid_pos_x;
+		new_instance.grid_pos_y = grid_pos_y;
+		add_child(new_instance);
+		
 		get_parent().clear_one_explosion()
 
 
